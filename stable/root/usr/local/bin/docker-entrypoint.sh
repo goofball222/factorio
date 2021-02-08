5eddde523ae380997ce6b2c8cd6a3d2ef1b1dc47
The Goofball <goofball222@gmail.com>
2021-02-08 16:31:28 -0700

    Copy functionized script updates from experimental to stable

--- stable/root/usr/local/bin/docker-entrypoint.sh
+++ stable/root/usr/local/bin/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 # Init script for Factorio headless server Docker container
 # License: Apache-2.0
-# Github: https://github.com/goofball222/factorio.git
-SCRIPT_VERSION="1.2.1"
-# Last updated date: 2019-09-27
+# Github: https://github.com/goofball222/factorio
+SCRIPT_VERSION="1.3.0"
+# Last updated date: 2021-01-29
 
 set -Eeuo pipefail
 
@@ -13,35 +13,7 @@ if [ "${DEBUG}" == 'true' ];
         set -x
 fi
 
-log() {
-    echo "$(date -u +%FT$(nmeter -d0 '%3t' | head -n1)) <docker-entrypoint> $*"
-}
-
-log "INFO - Script version ${SCRIPT_VERSION}"
-
-FACTORIO_GID=${FACTORIO_GID:-}
-FACTORIO_UID=${FACTORIO_UID:-}
-
-FACTORIO_OPTS="${FACTORIO_OPTS:-}"
-
-FACTORIO_PORT=${FACTORIO_PORT:-}
-
-FACTORIO_RCON_PASSWORD=${FACTORIO_RCON_PASSWORD:-}
-
-FACTORIO_RCON_PORT=${FACTORIO_RCON_PORT:-27015}
-
-FACTORIO_SCENARIO=${FACTORIO_SCENARIO:-}
-
-if [ ! -z "${FACTORIO_GID}" ]; then
-    log "INFO - FACTORIO_GID is set to '${FACTORIO_GID}'. Please update to the PGID env variable."
-    log "INFO - Automatically converting FACTORIO_GID to PGID."
-    PGID=${FACTORIO_GID}
-fi
-if [ ! -z "${FACTORIO_UID}" ]; then
-    log "INFO - FACTORIO_UID is set to '${FACTORIO_UID}'. Please update to the PUID env variable."
-    log "INFO - Automatically converting FACTORIO_UID to PUID."
-    PUID=${FACTORIO_UID}
-fi
+. /usr/local/bin/entrypoint-functions.sh
 
 BASEDIR="/opt/factorio"
 BINDIR=${BASEDIR}/bin
@@ -56,152 +28,13 @@ FACTORIO=${BINDIR}/x64/factorio
 VOLDIR="/factorio"
 VOLSAVEDIR=${VOLDIR}/saves
 
-# Make sure required directories exist in ${VOLDIR} - IE: new install with empty volume on host mapped over default volume
-[ -d ${VOLDIR}/config ] || mkdir -p ${VOLDIR}/config
-[ -d ${VOLDIR}/mods ] || mkdir -p ${VOLDIR}/mods
-[ -d ${VOLDIR}/saves ] || mkdir -p ${VOLDIR}/saves
-[ -d ${VOLDIR}/scenarios ] || mkdir -p ${VOLDIR}/scenarios
+f_log "INFO - Script version ${SCRIPT_VERSION}"
+f_log "INFO - Entrypoint functions version ${ENTRYPOINT_FUNCTIONS_VERSION}"
 
 cd ${BASEDIR}
 
-do_chown() {
-    if [ "${RUN_CHOWN}" == 'false' ]; then
-        if [ ! "$(stat -c %u ${BASEDIR})" = "${PUID}" ] || [ ! "$(stat -c %u ${VOLDIR})" = "${PUID}" ] \
-        || [ ! "$(stat -c %u ${CONFIGDIR})" = "${PUID}" ] || [ ! "$(stat -c %u ${DATADIR})" = "${PUID}" ] \
-        || [ ! "$(stat -c %u ${MODDIR})" = "${PUID}" ] || [ ! "$(stat -c %u ${SAVEDIR})" = "${PUID}" ] \
-        || [ ! "$(stat -c %u ${SCENARIODIR})" = "${PUID}" ]; then
-            log "WARN - Configured PUID doesn't match owner of a required directory. Ignoring RUN_CHOWN=false"
-            log "INFO - Ensuring permissions are correct before continuing - 'chown -R factorio:factorio ${BASEDIR}'"
-            log "INFO - Running recursive 'chown' on Docker overlay2 storage is **really** slow. This may take a bit."
-            chown -R factorio:factorio ${BASEDIR} ${VOLDIR}
-        else
-            log "INFO - RUN_CHOWN set to 'false' - Not running 'chown -R factorio:factorio ${BASEDIR}', assume permissions are right."
-        fi
-    else
-        log "INFO - Ensuring permissions are correct before continuing - 'chown -R factorio:factorio ${BASEDIR}'"
-        log "INFO - Running recursive 'chown' on Docker overlay2 storage is **really** slow. This may take a bit."
-        chown -R factorio:factorio ${BASEDIR} ${VOLDIR}
-    fi
-}
-
-load_save() {
-    # We avoid using --start-server-load-latest here, as the latest save might not be the desired one
-    # Ex: If you change from scenario to default gameplay but forgot to delete scenario.zip it would be loaded instead of save.zip
-    FACTORIO_OPTS="${FACTORIO_OPTS} --start-server ${SAVE_NAME} --server-settings ${CONFIGDIR}/server-settings.json --server-id ${CONFIGDIR}/server-id.json"
-}
-
-factorio_setup() {
-
-    if [ ! -z "${FACTORIO_PORT}" ];
-        then
-            FACTORIO_OPTS="${FACTORIO_OPTS} --port ${FACTORIO_PORT}"
-    fi
-
-    log "INFO - Remove any incomplete *.tmp.zip from crash/forced exit in ${SAVEDIR}"
-    rm -f ${SAVEDIR}/*.tmp.zip
-
-    if [ ! -z "${FACTORIO_RCON_PASSWORD}" ];
-        then
-            log "INFO - Using RCON password found in ENV"
-            FACTORIO_RCON_PASSWORD=${FACTORIO_RCON_PASSWORD}
-        else
-            # Check for RCON password file, generate random and set if doesn't exist
-            if [ ! -f "${CONFIGDIR}/RCON.pwd" ];
-                then
-                    log "INFO - No RCON.pwd found in ${CONFIGDIR}, generating random"
-                    set +o pipefail
-                    FACTORIO_RCON_PASSWORD=$(cat /dev/urandom | tr -dc 'a-f0-9' | head -c16)
-                    set -o pipefail
-                    echo "${FACTORIO_RCON_PASSWORD}" > "${CONFIGDIR}/RCON.pwd"
-                    chown factorio:factorio ${CONFIGDIR}/RCON.pwd
-                else
-                    log "INFO - Using existing RCON.pwd found in ${CONFIGDIR}"
-                    FACTORIO_RCON_PASSWORD=$(cat ${CONFIGDIR}/RCON.pwd)
-            fi
-    fi
-
-    log "INFO - RCON password is '${FACTORIO_RCON_PASSWORD}'"
-    FACTORIO_OPTS="${FACTORIO_OPTS} --rcon-port=${FACTORIO_RCON_PORT} --rcon-password ${FACTORIO_RCON_PASSWORD}"
-
-    # Copy example configs to CONFIGDIR
-    cp -p ${DATADIR}/server-settings.example.json ${CONFIGDIR}/server-settings.example.json
-    log "INFO - Copied latest server-settings.example.json to ${CONFIGDIR}"
-    cp -p ${DATADIR}/map-gen-settings.example.json ${CONFIGDIR}/map-gen-settings.example.json
-    log "INFO - Copied latest map-gen-settings.example.json to ${CONFIGDIR}"
-
-    # Copy example configs to working configuration if they don't exist
-    if [ ! -f "${CONFIGDIR}/server-settings.json" ];
-        then
-            log "WARN - No server-settings.json found in ${CONFIGDIR}, copying from example"
-            cp -p ${DATADIR}/server-settings.example.json ${CONFIGDIR}/server-settings.json
-        else
-            log "INFO - Using existing server-settings.json found in ${CONFIGDIR}"
-    fi
-
-    if [ ! -f "${CONFIGDIR}/map-gen-settings.json" ];
-        then
-            log "WARN - No map-gen-settings.json found in ${CONFIGDIR}, copying from example"
-            cp -p ${DATADIR}/map-gen-settings.example.json ${CONFIGDIR}/map-gen-settings.json
-        else
-            log "INFO - Using existing map-gen-settings.json found in ${CONFIGDIR}"
-    fi
-
-    # Check for banlist file in config dir, set launch options to use if found
-    if [ ! -f "${CONFIGDIR}/server-banlist.json" ];
-        then
-            log "INFO - No server-banlist.json found in ${CONFIGDIR}, ignoring"
-        else
-            log "INFO - Using server-banlist.json found in ${CONFIGDIR}"
-            FACTORIO_OPTS="${FACTORIO_OPTS} --server-banlist ${CONFIGDIR}/server-banlist.json"
-    fi
-
-    # Check for whitelist file in config dir, set launch options to use if found
-    if [ ! -f "${CONFIGDIR}/server-whitelist.json" ];
-        then
-            log "INFO - No server-whitelist.json found in ${CONFIGDIR}, ignoring"
-        else
-            log "INFO - Using server-whitelist.json found in ${CONFIGDIR}"
-            FACTORIO_OPTS="${FACTORIO_OPTS} --server-whitelist ${CONFIGDIR}/server-whitelist.json --use-server-whitelist"
-    fi
-
-    # Check for admin list file in config dir, set launch options to use if found
-    if [ ! -f "${CONFIGDIR}/server-adminlist.json" ];
-        then
-            log "INFO - No server-adminlist.json found in ${CONFIGDIR}, ignoring"
-        else
-            log "INFO - Using server-adminlist.json found in ${CONFIGDIR}"
-            FACTORIO_OPTS="${FACTORIO_OPTS} --server-adminlist ${CONFIGDIR}/server-adminlist.json"
-    fi
-
-    # Choose save file name. Scenario saves are named as the scenario itself.
-    if [ ! -z "${FACTORIO_SCENARIO}" ];
-        then
-            SAVE_NAME="${FACTORIO_SCENARIO}"
-        else
-            SAVE_NAME="save"
-    fi
-
-    # Check for existing save file, use if found. Generate new with settings if not.
-    if [ ! -f "${SAVEDIR}/${SAVE_NAME}.zip" ];
-        then
-            log "WARN - No ${SAVE_NAME}.zip found in ${SAVEDIR}"
-            log "INFO - Creating new map / ${SAVE_NAME}.zip in ${SAVEDIR} with settings from ${CONFIGDIR}/map-gen-settings.json"
-            if [ ! -z "${FACTORIO_SCENARIO}" ];
-                then
-                    FACTORIO_OPTS="${FACTORIO_OPTS} --start-server-load-scenario ${FACTORIO_SCENARIO} --server-settings ${CONFIGDIR}/server-settings.json --server-id ${CONFIGDIR}/server-id.json"
-                else
-                    su-exec factorio:factorio ${FACTORIO} --create ${VOLSAVEDIR}/save.zip --map-gen-settings ${CONFIGDIR}/map-gen-settings.json
-                    load_save
-            fi
-        else
-            log "INFO - Loading ${SAVE_NAME}.zip found in ${SAVEDIR}"
-            load_save
-    fi
-
-}
-
-exit_handler() {
-    log "INFO - Exit signal received, commencing shutdown"
+f_exit_handler() {
+    f_log "INFO - Exit signal received, commencing shutdown"
     pkill -15 -f ${FACTORIO}
     for i in `seq 0 9`;
         do
@@ -210,74 +43,63 @@ exit_handler() {
             [ $i -gt 8 ] && pkill -9 -f ${FACTORIO} || true
             sleep 1
     done
-    log "INFO - Shutdown complete. Nothing more to see here. Have a nice day!"
-    log "INFO - Exit with status code ${?}"
+    f_log "INFO - Shutdown complete. Nothing more to see here. Have a nice day!"
+    f_log "INFO - Exit with status code ${?}"
     exit ${?};
 }
 
-# Wait indefinitely on tail until killed
-idle_handler() {
+f_idle_handler() {
     while true
     do
         tail -f /dev/null & wait ${!}
     done
 }
 
-trap 'kill ${!}; exit_handler' SIGHUP SIGINT SIGQUIT SIGTERM
+trap 'kill ${!}; f_exit_handler' SIGHUP SIGINT SIGQUIT SIGTERM
 
-if [ "$(id -u)" = '0' ];
-    then
-        log "INFO - Entrypoint running with UID 0 (root)"
-        if [ "$(id factorio -g)" != "${PGID}" ] || [ "$(id factorio -u)" != "${PUID}" ];
-            then
-                log "INFO - Setting custom factorio GID/UID: GID=${PGID}, UID=${PUID}"
-                groupmod -o -g ${PGID} factorio
-                usermod -o -u ${PUID} factorio
-            else
-                log "INFO - GID/UID for factorio are unchanged: GID=${PGID}, UID=${PUID}"
-        fi
-
-        if [[ "${@}" == 'factorio' ]];
-            then
-                do_chown
-                factorio_setup
-                if [ "${RUNAS_UID0}" == 'true' ];
-                    then
-                       log "INFO - RUNAS_UID0 = 'true', running Factorio Headless as UID 0 (root)"
-                       log "WARN - ======================================================================"
-                       log "WARN - *** Running app as UID 0 (root) is an insecure configuration ***"
-                       log "WARN - ======================================================================"
-                       log "EXEC - ${FACTORIO} ${FACTORIO_OPTS}"
-                       exec 0<&-
-                       exec ${FACTORIO} ${FACTORIO_OPTS} &
-                       idle_handler
-                fi
-
-                log "INFO - Use su-exec to drop priveleges and start Factorio Headless as GID=${PGID}, UID=${PUID}"
-                log "EXEC - su-exec factorio:factorio ${FACTORIO} ${FACTORIO_OPTS}"
-                exec 0<&-
-                exec su-exec factorio:factorio ${FACTORIO} ${FACTORIO_OPTS} &
-                idle_handler
-            else
-                log "EXEC - ${@} as UID 0 (root)"
-                exec "${@}"
+if [ "$(id -u)" = '0' ]; then
+    f_log "INFO - Entrypoint running with UID 0 (root)"
+    if [[ "${@}" == 'factorio' ]]; then
+        f_giduid
+        f_chkdir
+        f_chown
+        f_setup
+        if [ "${RUNAS_UID0}" == 'true' ]; then
+            f_log "INFO - RUNAS_UID0 = 'true', running Factorio Headless as UID 0 (root)"
+            f_log "WARN - ======================================================================"
+            f_log "WARN - *** Running app as UID 0 (root) is an insecure configuration ***"
+            f_log "WARN - ======================================================================"
+            f_log "EXEC - ${FACTORIO} ${FACTORIO_OPTS}"
+            exec 0<&-
+            exec ${FACTORIO} ${FACTORIO_OPTS} &
+            f_idle_handler
+        else
+            f_log "INFO - Use su-exec to drop priveleges and start Factorio Headless as GID=${PGID}, UID=${PUID}"
+            f_log "EXEC - su-exec factorio:factorio ${FACTORIO} ${FACTORIO_OPTS}"
+            exec 0<&-
+            exec su-exec factorio:factorio ${FACTORIO} ${FACTORIO_OPTS} &
+            f_idle_handler
         fi
     else
-        log "WARN - Container/entrypoint not started as UID 0 (root)"
-        log "WARN - Unable to change permissions or set custom GID/UID if configured"
-        log "WARN - Process will be spawned with GID=$(id -g), PID=$(id -u)"
-        log "WARN - Depending on permissions requested command may not work"
-        if [[ "${@}" == 'factorio' ]];
-            then
-                factorio_setup
-                exec 0<&-
-                log "EXEC - ${FACTORIO} ${FACTORIO_OPTS}"
-                exec ${FACTORIO} ${FACTORIO_OPTS} &
-                idle_handler
-            else
-                log "EXEC - ${@}"
-                exec "${@}"
-        fi
+        f_log "EXEC - ${@} as UID 0 (root)"
+        exec "${@}"
+    fi
+else
+    f_log "WARN - Container/entrypoint not started as UID 0 (root)"
+    f_log "WARN - Unable to change permissions or set custom GID/UID if configured"
+    f_log "WARN - Process will be spawned with GID=$(id -g), PID=$(id -u)"
+    f_log "WARN - Depending on permissions requested command may not work"
+    if [[ "${@}" == 'factorio' ]]; then
+        f_chkdir
+        f_setup
+        f_log "EXEC - ${FACTORIO} ${FACTORIO_OPTS}"
+        exec 0<&-
+        exec ${FACTORIO} ${FACTORIO_OPTS} &
+        f_idle_handler
+    else
+        f_log "EXEC - ${@}"
+        exec "${@}"
+    fi
 fi
 
 # Script should never make it here, but just in case exit with a generic error code if it does
---
+++ stable/root/usr/local/bin/entrypoint-functions.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env bash
+
+# entrypoint-functions.sh script for Factorio headless server Docker container
+# License: Apache-2.0
+# Github: https://github.com/goofball222/factorio
+ENTRYPOINT_FUNCTIONS_VERSION="1.0.1"
+# Last updated date: 2021-01-29
+
+f_chkdir() {
+    # Make sure required directories exist in ${VOLDIR} - IE: new install with empty volume on host mapped over default volume
+    [ -d ${VOLDIR}/config ] || mkdir -p ${VOLDIR}/config
+    [ -d ${VOLDIR}/mods ] || mkdir -p ${VOLDIR}/mods
+    [ -d ${VOLDIR}/saves ] || mkdir -p ${VOLDIR}/saves
+    [ -d ${VOLDIR}/scenarios ] || mkdir -p ${VOLDIR}/scenarios
+}
+
+f_chown() {
+    if [ "${RUN_CHOWN}" == 'false' ]; then
+        if [ ! "$(stat -c %u ${BASEDIR})" = "${PUID}" ] || [ ! "$(stat -c %u ${VOLDIR})" = "${PUID}" ] \
+        || [ ! "$(stat -c %u ${CONFIGDIR})" = "${PUID}" ] || [ ! "$(stat -c %u ${DATADIR})" = "${PUID}" ] \
+        || [ ! "$(stat -c %u ${MODDIR})" = "${PUID}" ] || [ ! "$(stat -c %u ${SAVEDIR})" = "${PUID}" ] \
+        || [ ! "$(stat -c %u ${SCENARIODIR})" = "${PUID}" ]; then
+            f_log "WARN - Configured PUID doesn't match owner of a required directory. Ignoring RUN_CHOWN=false"
+            f_log "INFO - Ensuring permissions are correct before continuing - 'chown -R factorio:factorio ${BASEDIR}'"
+            f_log "INFO - Running recursive 'chown' on Docker overlay2 storage is **really** slow. This may take a bit."
+            chown -R factorio:factorio ${BASEDIR} ${VOLDIR}
+        else
+            f_log "INFO - RUN_CHOWN set to 'false' - Not running 'chown -R factorio:factorio ${BASEDIR}', assume permissions are right."
+        fi
+    elif [ "${RUNAS_UID0}" == 'true' ]; then
+        f_log "INFO - RUNAS_UID0=true - Not running 'chown -R factorio:factorio ${BASEDIR} ${VOLDIR}', no need to worry about permissions."
+    else
+        f_log "INFO - Ensuring permissions are correct before continuing - 'chown -R factorio:factorio ${BASEDIR}'"
+        f_log "INFO - Running recursive 'chown' on Docker overlay2 storage is **really** slow. This may take a bit."
+        chown -R factorio:factorio ${BASEDIR} ${VOLDIR}
+    fi
+}
+
+f_giduid() {
+    FACTORIO_GID=${FACTORIO_GID:-}
+    FACTORIO_UID=${FACTORIO_UID:-}
+    if [ ! -z "${FACTORIO_GID}" ]; then
+        f_log "INFO - FACTORIO_GID is set. Please use the updated PGID variable. Automatically converting to PGID."
+        PGID=${FACTORIO_GID}
+    fi
+    if [ ! -z "${FACTORIO_UID}" ]; then
+        f_log "INFO - FACTORIO_UID is set. Please use the updated PUID variable. Automatically converting to PUID."
+        PUID=${FACTORIO_UID}
+    fi
+    if [ "$(id factorio -g)" != "${PGID}" ] || [ "$(id factorio -u)" != "${PUID}" ]; then
+        f_log "INFO - Setting custom factorio GID/UID: GID=${PGID}, UID=${PUID}"
+        groupmod -o -g ${PGID} factorio
+        usermod -o -u ${PUID} factorio
+    else
+        f_log "INFO - GID/UID for factorio are unchanged: GID=${PGID}, UID=${PUID}"
+    fi
+}
+
+f_load_save() {
+    # We avoid using --start-server-load-latest here, as the latest save might not be the desired one
+    # Ex: If you change from scenario to default gameplay but forgot to delete scenario.zip it would be loaded instead of save.zip
+    FACTORIO_OPTS="${FACTORIO_OPTS} --start-server ${SAVE_NAME} --server-settings ${CONFIGDIR}/server-settings.json --server-id ${CONFIGDIR}/server-id.json"
+}
+
+f_log() {
+    echo "$(date -u +%FT$(nmeter -d0 '%3t' | head -n1)) <docker-entrypoint> $*"
+}
+
+f_setup() {
+    FACTORIO_OPTS="${FACTORIO_OPTS:-}"
+    FACTORIO_PORT=${FACTORIO_PORT:-}
+    FACTORIO_RCON_PASSWORD=${FACTORIO_RCON_PASSWORD:-}
+    FACTORIO_RCON_PORT=${FACTORIO_RCON_PORT:-27015}
+    FACTORIO_SCENARIO=${FACTORIO_SCENARIO:-}
+    if [ ! -z "${FACTORIO_PORT}" ]; then
+        FACTORIO_OPTS="${FACTORIO_OPTS} --port ${FACTORIO_PORT}"
+    fi
+
+    f_log "INFO - Remove any incomplete *.tmp.zip from crash/forced exit in ${SAVEDIR}"
+    rm -f ${SAVEDIR}/*.tmp.zip
+
+    if [ ! -z "${FACTORIO_RCON_PASSWORD}" ]; then
+        f_log "INFO - Using RCON password found in ENV"
+        FACTORIO_RCON_PASSWORD=${FACTORIO_RCON_PASSWORD}
+    else
+        # Check for RCON password file, generate random and set if doesn't exist
+        if [ ! -f "${CONFIGDIR}/RCON.pwd" ]; then
+            f_log "INFO - No RCON.pwd found in ${CONFIGDIR}, generating random"
+            set +o pipefail
+            FACTORIO_RCON_PASSWORD=$(cat /dev/urandom | tr -dc 'a-f0-9' | head -c16)
+            set -o pipefail
+            echo "${FACTORIO_RCON_PASSWORD}" > "${CONFIGDIR}/RCON.pwd"
+            chown factorio:factorio ${CONFIGDIR}/RCON.pwd
+        else
+            f_log "INFO - Using existing RCON.pwd found in ${CONFIGDIR}"
+            FACTORIO_RCON_PASSWORD=$(cat ${CONFIGDIR}/RCON.pwd)
+        fi
+    fi
+
+    f_log "INFO - RCON password is '${FACTORIO_RCON_PASSWORD}'"
+    FACTORIO_OPTS="${FACTORIO_OPTS} --rcon-port=${FACTORIO_RCON_PORT} --rcon-password ${FACTORIO_RCON_PASSWORD}"
+
+    # Copy example configs to CONFIGDIR
+    cp -p ${DATADIR}/server-settings.example.json ${CONFIGDIR}/server-settings.example.json
+    f_log "INFO - Copied latest server-settings.example.json to ${CONFIGDIR}"
+    cp -p ${DATADIR}/map-gen-settings.example.json ${CONFIGDIR}/map-gen-settings.example.json
+    f_log "INFO - Copied latest map-gen-settings.example.json to ${CONFIGDIR}"
+
+    # Copy example configs to working configuration if they don't exist
+    if [ ! -f "${CONFIGDIR}/server-settings.json" ]; then
+        f_log "WARN - No server-settings.json found in ${CONFIGDIR}, copying from example"
+        cp -p ${DATADIR}/server-settings.example.json ${CONFIGDIR}/server-settings.json
+    else
+        f_log "INFO - Using existing server-settings.json found in ${CONFIGDIR}"
+    fi
+
+    if [ ! -f "${CONFIGDIR}/map-gen-settings.json" ]; then
+        f_log "WARN - No map-gen-settings.json found in ${CONFIGDIR}, copying from example"
+        cp -p ${DATADIR}/map-gen-settings.example.json ${CONFIGDIR}/map-gen-settings.json
+    else
+        f_log "INFO - Using existing map-gen-settings.json found in ${CONFIGDIR}"
+    fi
+
+    # Check for banlist file in config dir, set launch options to use if found
+    if [ ! -f "${CONFIGDIR}/server-banlist.json" ]; then
+        f_log "INFO - No server-banlist.json found in ${CONFIGDIR}, ignoring"
+    else
+        f_log "INFO - Using server-banlist.json found in ${CONFIGDIR}"
+        FACTORIO_OPTS="${FACTORIO_OPTS} --server-banlist ${CONFIGDIR}/server-banlist.json"
+    fi
+
+    # Check for whitelist file in config dir, set launch options to use if found
+    if [ ! -f "${CONFIGDIR}/server-whitelist.json" ]; then
+        f_log "INFO - No server-whitelist.json found in ${CONFIGDIR}, ignoring"
+    else
+        f_log "INFO - Using server-whitelist.json found in ${CONFIGDIR}"
+        FACTORIO_OPTS="${FACTORIO_OPTS} --server-whitelist ${CONFIGDIR}/server-whitelist.json --use-server-whitelist"
+    fi
+
+    # Check for admin list file in config dir, set launch options to use if found
+    if [ ! -f "${CONFIGDIR}/server-adminlist.json" ]; then
+        f_log "INFO - No server-adminlist.json found in ${CONFIGDIR}, ignoring"
+    else
+        f_log "INFO - Using server-adminlist.json found in ${CONFIGDIR}"
+        FACTORIO_OPTS="${FACTORIO_OPTS} --server-adminlist ${CONFIGDIR}/server-adminlist.json"
+    fi
+
+    # Choose save file name. Scenario saves are named as the scenario itself.
+    if [ ! -z "${FACTORIO_SCENARIO}" ]; then
+        SAVE_NAME="${FACTORIO_SCENARIO}"
+    else
+        SAVE_NAME="save"
+    fi
+
+    # Check for existing save file, use if found. Generate new with settings if not.
+    if [ ! -f "${SAVEDIR}/${SAVE_NAME}.zip" ]; then
+        f_log "WARN - No ${SAVE_NAME}.zip found in ${SAVEDIR}"
+        f_log "INFO - Creating new map / ${SAVE_NAME}.zip in ${SAVEDIR} with settings from ${CONFIGDIR}/map-gen-settings.json"
+        if [ ! -z "${FACTORIO_SCENARIO}" ]; then
+            FACTORIO_OPTS="${FACTORIO_OPTS} --start-server-load-scenario ${FACTORIO_SCENARIO} --server-settings ${CONFIGDIR}/server-settings.json --server-id ${CONFIGDIR}/server-id.json"
+        else
+            su-exec factorio:factorio ${FACTORIO} --create ${VOLSAVEDIR}/save.zip --map-gen-settings ${CONFIGDIR}/map-gen-settings.json
+            f_load_save
+        fi
+    else
+        f_log "INFO - Loading ${SAVE_NAME}.zip found in ${SAVEDIR}"
+        f_load_save
+    fi
+
+}
